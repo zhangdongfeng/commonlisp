9b8d9804f7a8cbe2b17e5951b1ee91290de697a1
zhangdf <zhangdf@actions-semi.com>
2017-12-25 19:29:31 +0800

    gerrit

--- mygit/src/gerrit.lisp
+++ mygit/src/gerrit.lisp
@@ -1,14 +1,5 @@
 (in-package :mygit)
 
-
-(defparameter *username* "zhangdongfeng"
-  "Username to use for API calls")
-
-(defparameter *password* "zhangdf1"
-  "Password to use for API calls")
-
-(defvar *url* nil)
-
 (define-condition api-error (error)
   ((http-status :initarg :http-status
                 :reader error-http-status)
@@ -17,45 +8,69 @@
    (response :initarg :response
              :reader error-response))
   (:report (lambda (c stream)
-             (format stream "github API error, HTTP status code ~A~%~A~@[~%~A~]"
+             (format stream "gerrit API error, HTTP status code ~A~%~A~@[~%~A~]"
                      (error-http-status c)
                      (error-http-headers c)
                      (error-response c)))))
 
-(defun keyword-to-github-keyword (keyword)
+(defun keyword-to-gerrit-keyword (keyword)
   (string-downcase (string keyword)))
 
-(defun github-keyword-to-keyword (string)
+(defun gerrit-keyword-to-keyword (string)
   (let ((*package* (find-package :keyword)))
     (read-from-string string)))
 
 (defun plist-to-http-parameters (plist)
   (loop
      for (key value) on plist by #'cddr
-     collect (cons (keyword-to-github-keyword key) value)))
+     collect (cons (keyword-to-gerrit-keyword key) value)))
 
 (defun plist-to-hash-table (plist)
   (loop
      with hash-table = (make-hash-table :test #'equal)
      for (key value) on plist by #'cddr
-     do (setf (gethash (keyword-to-github-keyword key) hash-table) value)
+     do (setf (gethash (keyword-to-gerrit-keyword key) hash-table) value)
      finally (return hash-table)))
 
+#+ (or)
 (defun api-command (url &key body (method :get) (username *username*) (password *password*) parameters)
   (multiple-value-bind
         (body status-code headers)
-      (drakma:http-request (format nil "https://api.github.com~A" url)
+      (drakma:http-request (format nil (concatenate 'string *host* "~A" )  url )
                            :method method
                            :parameters (plist-to-http-parameters parameters)
                            :basic-authorization (when username (list username password))
                            :content-type "application/json"
+                           :force-binary t
                            :content (when body
                                       (with-output-to-string (s)
                                         (yason:encode (plist-to-hash-table body) s))))
     (let* ((yason:*parse-object-as* :plist)
-           (yason:*parse-object-key-fn* 'github-keyword-to-keyword)
+           (yason:*parse-object-key-fn* #'gerrit-keyword-to-keyword)
            (response (when body
                        (yason:parse (flex:octets-to-string body :external-format :utf-8)))))
+      (if (< status-code 300)
+          (values response headers)
+          (error 'api-error
+                 :http-status status-code
+                 :http-headers headers
+                 :response response)))))
+
+(defun api-command (url &key body (method :get) (username *username*) (password *password*) parameters)
+  (multiple-value-bind
+        (body status-code headers)
+      (drakma:http-request (format nil (concatenate 'string *host* "~A" )  url )
+                           :method method
+                           :parameters (plist-to-http-parameters parameters)
+                           :basic-authorization (list "zhangdf" "javateam1!")
+                           :content-type "application/json"
+                           :force-binary t
+                           :content (when body
+                                      (with-output-to-string (s)
+                                        (yason:encode (plist-to-hash-table body) s))))
+    (let* ((yason:*parse-object-as* :plist)
+           (yason:*parse-object-key-fn* #'gerrit-keyword-to-keyword)
+           (response (flex:octets-to-string body :external-format :utf-8)))
       (if (< status-code 300)
           (values response headers)
           (error 'api-error
@@ -75,7 +90,7 @@
                               value)
                           result)))))
 
-(defmacro define-github-command (name parameters &body body)
+(defmacro define-gerrit-command (name parameters &body body)
   ;; unhygienic
   `(prog1
        (defun ,name (&rest parameters &key ,@parameters)
@@ -83,13 +98,12 @@
                                              collect (if (listp parameter) (first parameter) parameter))))
          ,@body)
      (export ',name)))
+(defparameter *username* "zhangdf"
+  "Username to use for API calls")
+(defparameter *password* "javateam1!"
+  "Password to use for API calls")
+(defparameter *host* "http://192.168.4.4:8080")
 
-(define-github-command create-repository (name org description homepage public has-issues has-wiki has-downloads)
-  (booleanize-parameters parameters :has-issues :has-wiki :has-downloads)
-  (api-command (if org (format nil "/orgs/~A/repos" org) "/user/repos")
-               :method :post
-               :body parameters))
-
-(define-github-command list-repositories (org)
-  (api-command (if org (format nil "/orgs/~A/repos" org) "/user/repos")
-               :method :get))
+(define-gerrit-command list-repositories (org)
+  (api-command "/projects/"
+               :method :get)  )
