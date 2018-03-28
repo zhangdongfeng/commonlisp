ffe06737c00b93b057831706b943d4adbb7d0c75
zhangdf <zhangdf@actions-semi.com>
2018-03-28 20:51:28 +0800

    refactor

--- elf-parser/test.lisp
+++ elf-parser/test.lisp
@@ -1,7 +1,5 @@
 (in-package :elf-parser)
 
-
-
 (defun show-static-symbols ()
   "Show all symbols in ELF in a manner similar to readelf."
   (let ((symtab (elf:named-section *elf* ".symtab")))
@@ -45,13 +43,13 @@
 
 (defun show-memory-layout ()
   "Show the layout of the elements of an elf file with binary offset."
-  (format t "~&addr     contents          end        size ~%")
+  (format t "~&addr     contents                  size ~%")
   (format t "--------------------------------------------~%")
   (with-slots (elf:sections elf:section-table elf:program-table) *elf*
     (mapc
      #'(lambda (trio)
          (metabang-bind:bind (((beg size name) trio))
-           (format t "~&0x~x ~18a 0x~x  ~D  ~%" beg name (+ beg size) size )))
+           (format t "~&0x~x ~18a   ~D  ~%" beg name  size )))
      (stable-sort
       (remove-if
        #'(lambda (trio) (zerop (second trio)))
@@ -65,27 +63,9 @@
       #'< :key #'car)))
   nil)
 
-#+ (or)
-(defun dump-symbol-list (sym-list)
-  (loop for sym in sym-list
-     do (format t "~&    ~8x ~5d ~8a ~6a ~a~%"
-                (elf:value sym) (elf:size sym) (elf:type sym)
-                (elf:binding sym)
-                (elf:sym-name sym))))
-(defparameter *get-all-text-symbols*
-  (lambda ()
-    (get-all-file-symbols '(dw_tag_subprogram))))
-
-(defparameter *get-all-data-symbols*
-  (lambda ()
-    (get-all-file-symbols '(dw_tag_variable))))
 
 (defparameter *overlay-pred* #'overlay-sym?)
 (defparameter *rodata-pred* #'rodata-sym?)
-
-
-
-
 
 (defun overlay-sym? (file-sym)
   (let* ((sym (elf-sym file-sym))
@@ -113,24 +93,29 @@
       (and (>= (elf:value sym) rodata-start)
            (<= (elf:value sym) rodata-end)))))
 
-(defun sum-syms (syms)
-  (loop for s in syms
-     sum (elf:size (elf-sym s))))
+(defun code-sym? (file-sym)
+  (eql :func (elf:type (elf-sym file-sym))))
+
+(defun data-sym?  (file-sym)
+  (and
+   (eql :object (elf:type (elf-sym file-sym)))
+   (not (rodata-sym? file-sym))
+   (not (overlay-sym? file-sym))))
+
 
 (defun calc-size (syms)
-  (let* ((code (remove-if-not
-                #'(lambda (s) (eql :func (elf:type (elf-sym s))))
-                syms))
-         (data (remove-if-not
-                #'(lambda (s) (eql :object (elf:type (elf-sym s))))
-                syms))
-         (rodata (remove-if-not  #'rodata-sym?  data) )
-         (overlay (remove-if-not  #'overlay-sym?  data)))
-    (let ((c (sum-syms code))
-          (r (sum-syms rodata) )
-          (o (sum-syms overlay) )
-          (d (sum-syms data)))
-      (values c (- d r  o) r o))))
+  (flet ((sum-syms (syms)
+           (loop for s in syms
+              sum (elf:size (elf-sym s)))))
+    (let* ((code (remove-if-not  #'code-sym? syms))
+           (data (remove-if-not  #'data-sym? syms))
+           (rodata (remove-if-not  #'rodata-sym?  syms) )
+           (overlay (remove-if-not  #'overlay-sym?  syms)))
+      (let ((c (sum-syms code))
+            (r (sum-syms rodata) )
+            (o (sum-syms overlay) )
+            (d (sum-syms data)))
+        (values c d  r o)))))
 
 (defun find-module-symbols  (module  all-syms)
   (remove-if-not #'(lambda (x )
@@ -140,15 +125,25 @@
   (remove-if #'(lambda (x )
                  (search   module  (file-name x))) all-syms))
 
-(defun dump-file-syms  (sym-list)
-  (loop for file-sym in sym-list
-     do (let ((sym (elf-sym file-sym)))
-          (format t "~&    ~8x ~5d ~8a ~6a ~a~%"
-                  (elf:value sym) (elf:size sym) (elf:type sym)
-                  (elf:binding sym)
-                  (elf:sym-name sym)))))
+(defun dump-file-syms  (sym-list &key (dump-code-symbol nil)
+                                   (dump-data-symbol nil)
+                                   (dump-rodata-symbol nil)
+                                   (dump-overlay-symbol nil)
+                                   &allow-other-keys)
+  (flet ((dump-sym (file-sym)
+           (let ((sym (elf-sym file-sym)))
+             (format t "~&    ~8x ~5d ~8a ~6a ~a~%"
+                     (elf:value sym) (elf:size sym) (elf:type sym)
+                     (elf:binding sym)
+                     (elf:sym-name sym)))))
+    (loop for sym in sym-list
+       do (cond
+            ((and  (rodata-sym? sym) dump-rodata-symbol) (dump-sym sym))
+            ((and  (data-sym? sym) dump-data-symbol) (dump-sym sym))
+            ((and  (code-sym? sym) dump-code-symbol) (dump-sym sym))
+            ((and  (overlay-sym? sym) dump-overlay-symbol) (dump-sym sym))))))
 
-(defun dump-module-file (module-syms   (dump-symbol nil) )
+(defun dump-module-file (module-syms &rest rest  &key (prefix "")  &allow-other-keys)
   (flet ((get-all-filename (syms)
            (let* ((all-path (mapcar #'file-name  syms)))
              (delete-duplicates all-path :test #'string=))))
@@ -156,15 +151,19 @@
       (format t "~&**** module details:~%")
       (format t "~&| file name |code size|data size|rodata size| overlay-data-size|~%")
       (dolist (f file-name)
-        (let* ((syms  (remove-if-not #'(lambda (s) (string= f (file-name s))) module-syms)))
-          (format t "~&| ~a~{~T|~:D~}|~%" f (multiple-value-list (calc-size syms)))
-          (if dump-symbol
-              (dump-file-syms syms)))))))
+        (let* ((syms  (remove-if-not #'(lambda (s) (string= f (file-name s))) module-syms))
+               (short-name (if (search prefix f) (subseq f (length prefix)) f)))
+          (format t "~&| ~a~{~T|~D~}|~%"
+                  short-name  (multiple-value-list (calc-size syms)))
+          (apply #'dump-file-syms syms rest))))))
 
-(defun show-debug-module-symbols (modules &optional &key
-                                                      (prefix "")  (dump-file nil) (dump-symbol nil)
-                                                      (rodata-pred #'rodata-sym?)
-                                                      (overlay-pred *overlay-pred*))
+(defun show-debug-module-symbols
+    (modules &rest rest &key (dump-file nil)
+                          (prefix "")   (dump-symbol nil)
+                          dump-code-symbol
+                          dump-data-symbol
+                          dump-overlay-symbol
+                          dump-rodata-symbol)
   "show  debug symbols info in dwarf .debug_info section
 threshod: optional, the threshold size to dump info
 dump-file: should also dump file info
@@ -177,7 +176,7 @@ prefix: path filter"
                (format t "~&|~18a~{~T|~:D~}|~%"  m
                        (multiple-value-list (calc-size module-syms)))
                (if dump-file
-                   (dump-module-file module-syms  :prefix prefix  :dump-symbol dump-symbol))
+                   (apply  #'dump-module-file module-syms rest))
                (setq all-syms (remove-module-symbols m all-syms)))))
       (format t "~&*** module mem :~%")
       (format t "~&| module name |code size|data size|rodata size|overlay data size|~%")
@@ -185,7 +184,8 @@ prefix: path filter"
       (format t "~&|~18a~{~T|~:D~}|~%"  "others"
               (multiple-value-list (calc-size all-syms)))
       (if dump-file
-          (dump-module-file all-syms  :prefix prefix  :dump-symbol dump-symbol)))))
+          (apply  #'dump-module-file all-syms rest))
+      )))
 
 
 
@@ -218,7 +218,6 @@ dump-symbol:  dump  symbols details in file
              do (format t "~&~a~%" s))
           (read-elf file)
           (when *elf*
-            (show-memory-sec '( "LOAD" "text" "rodata" "datas" "bss" "noinit"))
             (show-debug-module-symbols
              modules
              :prefix prefix
@@ -228,3 +227,23 @@ dump-symbol:  dump  symbols details in file
 
 #+(or)
 (sb-ext:save-lisp-and-die #p "elf-parser" :toplevel #'main :executable t)
+
+
+#+ (or)
+(show-debug-module-symbols '(
+                             "libsystem/service"
+                             "mqtt/"
+                             "libsystem/library"
+                             "actions/porting/"
+                             "wifi/host"
+                             "subsys/bluetooth"
+                             "/drivers/wifi/"
+                             "drivers"
+                             "subsys/net"
+                             "/samples/storyapp/"
+                             "/kernel/"
+                             "nofile")
+                           :dump-file nil
+                           :dump-data-symbol nil
+                           :dump-overlay-symbol t
+                           :prefix "/home/local/ACTIONS/zhangdf/sdk/GL5118_WIFI")
