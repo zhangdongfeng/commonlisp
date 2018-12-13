b69f87e3f2ec878261893ca2274a3f22590cc473
zhangdf <zhangdf@actions-semi.com>
2018-12-13 10:38:17 +0800

    nm

--- elf-parser/nm.lisp
+++ elf-parser/nm.lisp
@@ -106,30 +106,12 @@
         (make-instance
          'obj-symbol :obj-name obj-name :t-sym t-syms :u-sym u-syms))))
 
-(defun resolve-syms (obj  t-syms)
-  (declare (optimize debug))
-  (let ((u-syms (u-sym obj)))
-    (loop for usym in u-syms
-       collect (let ((found (find (car usym) t-syms
-                                  :test #'string= :key #'car)))
-                 (cons usym found)))))
-
-(defun analyse-module-dependency (obj-syms)
-  (declare (optimize debug))
-  (let ((t-syms (get-all-t-syms obj-syms)))
-    (loop for obj in obj-syms
-       collect (cons (obj-name obj) (resolve-syms obj t-syms)))))
-
-
-(defun get-all-t-syms (obj-syms)
-  (reduce #'append (mapcar #'t-sym obj-syms)))
-
 (defun get-nm-from-cmdline (cmd)
   (let* ((lines (string-to-lines cmd))
          (lines (remove-if  #'(lambda(l)(scan "built-in\\.o" l))
                             lines)))
     (trivial-shell:shell-command
-     (concatenate 'string "arm-none-eabi-nm "
+     (concatenate 'string *nm-cmd*
                   (reduce #'(lambda (a b) (concatenate 'string a " " b))
                           lines )))))
 
@@ -138,36 +120,161 @@
          (objs (mapcar #'parse-obj-symbol-from-string obj-nm)))
     objs))
 
-(defun merge-objs (objs)
-  (let* ((usym (reduce #'append (loop for i in objs collect (u-sym i))))
-         (tsym (reduce #'append (loop for i in objs collect  (t-sym i))))
-         (usym-new (remove-if #'(lambda (s)
-                                  (find (car s) tsym :test #'string= :key #'car))
-                              usym)))
-    (make-instance 'obj-symbol :obj-name (directory-namestring (obj-name (car objs)))
-                   :t-sym tsym :u-sym usym-new)))
+
+
+
+(defclass component ()
+  ((name  :initarg :name
+          :accessor  name)
+   (objs  :initarg :objs
+          :accessor  objs)
+   (resolved-objs  :initarg :resolved-objs
+                   :accessor  resolved-objs)
+   (usyms  :initarg :usyms
+           :accessor  usyms)
+   (rsyms  :initarg :rsyms
+           :accessor  rsyms)
+   (tsyms  :initarg :tsyms
+           :accessor  tsyms)))
+
+(defclass resolved-symbol ()
+  ((name  :initarg :name
+          :accessor  name)
+   (ref-module  :initarg :ref-module
+                :accessor  ref-module)
+   (def-module :initarg :def-module
+     :accessor def-module)))
 
 (defun get-component-from-objs (objs)
   (let* ((non-empty-objs (remove-if-not #'(lambda (o) (or (t-sym o) (u-sym o))) objs))
-         (paths (mapcar #'(lambda (f) (directory-namestring (obj-name f)))
-                        non-empty-objs))
-         (com-name (remove-duplicates paths :test #'string=))
-         (components (mapcar #'(lambda (name)
-                                 (remove-if-not
-                                  #'(lambda (obj)
-                                      (let ((obj-dir (directory-namestring (obj-name obj))))
-                                        (string=  name obj-dir)))
-                                  non-empty-objs))
-                             com-name)))
-    (mapcar #'merge-objs components)))
+         (paths (remove-duplicates
+                 (mapcar #'(lambda (f) (directory-namestring (obj-name f)))
+                         non-empty-objs)
+                 :test #'string=))
+         (path-objs (mapcar #'(lambda (name)
+                                (remove-if-not
+                                 #'(lambda (obj)
+                                     (let ((obj-dir (directory-namestring (obj-name obj))))
+                                       (string=  name obj-dir)))
+                                 non-empty-objs))
+                            paths))
+         (components (mapcar
+                      #'(lambda (name objs) (make-instance 'component :name name :objs objs))
+                      paths path-objs)))
+    (mapcar #'component-merge-objs  components)
+    components))
 
-(defun get-dependency-count (d)
-  (let* ((usyms (remove-if #'cdr (cdr d)))
-         (syms (remove-if-not #'cdr (cdr d)))
-         (syms (remove-duplicates syms :key #'(lambda (s)
-                                                (directory-namestring (cddr s)))
-                                  :test #'string=)))
-    (list (length syms) (length usyms))))
+
+(defun resolve-obj (obj  syms)
+  (let ((usyms (u-sym obj))
+        (resolved nil))
+    (dolist (u usyms)
+      (let ((found (find (car u) syms
+                         :test #'string= :key #'car)))
+        (if found
+            (push (make-instance 'resolved-symbol
+                                 :name (car u) :ref-module (cdr u) :def-module  (cdr found)) resolved))))
+    (cons (obj-name obj) resolved)))
+
+(defmethod component-merge-objs ( (c component))
+  (let* ((objs  (objs c))
+         (usym (reduce #'append (loop for i in objs collect (u-sym i))))
+         (tsym (reduce #'append (loop for i in objs collect  (t-sym i))))
+         (usym-new (remove-if #'(lambda (s)
+                                  (find (car s) tsym :test #'string= :key #'car))
+                              usym))
+         (resolve-objs nil))
+    (dolist (o objs)
+      (push (resolve-obj o tsym) resolve-objs))
+    (setf (tsyms c) tsym
+          (usyms c) usym-new
+          (resolved-objs c) resolve-objs)))
+
+
+(defmethod component-resolve-syms ( (c component)  t-syms)
+  (declare (optimize debug))
+  (let ((u-syms (usyms c))
+        (resolved nil)
+        (unresolved nil))
+    (dolist (usym u-syms)
+      (let ((found (find (car usym) t-syms
+                         :test #'string= :key #'car)))
+        (if found
+            (push (make-instance 'resolved-symbol
+                                 :name (car usym) :ref-module (cdr usym) :def-module  (cdr found)) resolved)
+            (push usym unresolved))))
+    (setf (usyms c) unresolved
+          (rsyms c) resolved)
+    c))
+
+(defmethod component-get-depend-components ( (c component))
+  (let* ((module-names (mapcar #'def-module (rsyms c)))
+         (component-names (mapcar #'directory-namestring module-names) ))
+    (remove-duplicates component-names
+                       :test #'string=)))
+
+(defmethod component-get-depend-modules ( (c component))
+  (let ((module-names (mapcar #'def-module (rsyms c))))
+    (remove-duplicates module-names
+                       :test #'string=)))
+
+(defmethod component-get-modules ( (c component))
+  (let ((module-names (mapcar #'ref-module (rsyms c))))
+    (remove-duplicates module-names :test #'string=)))
+
+(defmethod component-get-depend-components-of ( (c component) ref-module)
+  (declare (optimize debug))
+  (let* ((syms (rsyms c))
+         (syms (remove-if-not  #'(lambda (s) (string= ref-module (ref-module s))) syms))
+         (modules (mapcar #'def-module syms))
+         (modules (mapcar #'directory-namestring modules)))
+    (remove-duplicates  modules :test #'string=)))
+
+(defun get-module-shortname (full-name)
+  (let* ((mod-dir (reverse (pathname-directory full-name)))
+         (mod-name (file-namestring full-name))
+         (mod-dir (if (cadr mod-dir)
+                      (concatenate 'string  (cadr mod-dir) "/" (car mod-dir))
+                      (car mod-dir))))
+    (concatenate 'string mod-dir "/" mod-name)))
+
+(defmethod component-dump ( (c component))
+  (declare (optimize debug))
+  (let* ( (c-name (name c))
+         (usyms (usyms c))
+          (syms (rsyms c))
+          (objs  (resolved-objs c))
+          (depend-components (component-get-depend-components c))
+          (depend-modules (component-get-depend-modules c))
+          (modules (component-get-modules c)))
+    (format t  "~%~a~%" c-name)
+    (format t  "depends modules: ~d  unresolved symbols: ~d ~%"
+            (length depend-components) (length usyms))
+    (format t  "~{~a~%~} ~%" (mapcar #'get-module-shortname depend-components))
+    ;;(format t  "~{~a~%~} ~%" (mapcar #'get-module-shortname depend-modules))
+    (dolist (m modules )
+      (format t  "~a  ~%~{~a~%~} ~%" (file-namestring m)
+              (mapcar #'get-module-shortname (component-get-depend-modules-of c m)) ))
+    (format t  "component internal symbols:~%")
+    (dolist (o objs)
+      (format t "~a ~%" (file-namestring (car o)))
+      (let ((syms (cdr o)))
+        (dolist (s syms)
+          (format t  "~a  ==>  ~a ~%" (name s) (file-namestring (def-module s))))))
+    (format t  "~%component  symbols:~%")
+    (dolist (s syms)
+      (format t  "~a/~a  ==>  ~a ~%" (name s)  (file-namestring (ref-module s))
+              (get-module-shortname (def-module s))))
+    #+ (or)
+    (progn
+      (format t  "~%component  undefined symbols:~%")
+      (dolist (u usyms)
+        (format t  "~a from ~a  undefined ~%"  (car u)  (file-namestring (cdr u)))))))
+
+(defun analyse-module-dependency (components)
+  (declare (optimize debug))
+  (let ((t-syms  (reduce #'append (mapcar #'tsyms components))))
+    (mapcar #'(lambda (c) (component-resolve-syms c t-syms)) components)))
 
 (defun get-component-dependency  (path)
   (let* ((cmd (read-file-into-string path ))
@@ -177,37 +284,33 @@
     (sort dependency  #'> :key #'(lambda (d)
                                    (car (get-dependency-count d))))))
 
-(defun dump-dependency (d)
-  (let* ((c-name (car d))
-         (syms (remove-if-not #'cdr (cdr d))))
-    (format t  "~a~%" c-name)
-    (dolist (s syms)
-      (let* ((sym (car s))
-             (mod-dir (reverse (pathname-directory (cddr s))))
-             (mod-name (if (cadr mod-dir) (concatenate 'string  (cadr mod-dir) "/" (car mod-dir))  (car mod-dir)))
-             (namestring (directory-namestring (cddr s))))
-        (format t "~a   ~a~%" (car sym) namestring)))))
-
-
 (defun show-component-dependency (dep)
-  (let ((dep (stable-sort dep #'string> :key #'car)))
-    (dolist (d dep)
-      (let* ((c-name (car d))
-             (syms (remove-if-not #'cdr (cdr d)))
-             (syms (remove-duplicates syms :key #'(lambda (s)
-                                                    (directory-namestring (cddr s)))
-                                      :test #'string=)))
-        (format t  "~a ~d ~d ~%" c-name (car (get-dependency-count d))
-                (cadr (get-dependency-count d)))
-        (dolist (s syms)
-          (let* ((sym (car s))
-                 (mod-dir (reverse (pathname-directory (cddr s))))
-                 (mod-name (if (cadr mod-dir) (concatenate 'string  (cadr mod-dir) "/" (car mod-dir))  (car mod-dir))))
-            (format t "~a~%"  mod-name)))))))
+  (let ((dep (stable-sort dep #'string> :key #'name)))
+    #+ (or)
+    (dolist (c dep)
+      (let* ( (c-name (name c))
+             (usyms (usyms c))
+              (syms (rsyms c))
+              (depend-components (component-get-depend-components c))
+              (depend-modules (component-get-depend-modules c))
+              (modules (component-get-modules c)))
+        (format t  "~a  ~%" c-name)
+        (format t  "~%depends modules: ~d  unresolved symbols: ~d ~%"
+                (length depend-components) (length usyms))
+        (format t  "~{~a~%~} ~%" (mapcar #'get-module-shortname depend-components))
+        ;;(format t  "~{~a~%~} ~%" (mapcar #'get-module-shortname depend-modules))
+        (dolist (m modules )
+          (format t  "~a  ~%~{~a~%~} ~%" (file-namestring m)
+                  (mapcar #'get-module-shortname (component-get-depend-modules-of c m)) ))))
+    (dolist (c dep)
+      (component-dump c))))
 
-#+or
-(defparameter *d* (get-component-dependency "/Users/zhangdongfeng/Downloads/ATS350B/cmd.txt"))
+(defparameter *nm-cmd* "/opt/mips-2014.11/bin/mips-sde-elf-nm ")
 
+#+ (or)
+(defparameter *d* (get-component-dependency "/home/local/ACTIONS/zhangdf/sdk/5211/gs700e/android/android/out/target/product/s700_cb7/obj/STATIC_LIBRARIES/cmd.txt"))
+#+ (or)
+(defparameter *d* (get-component-dependency "/home/local/ACTIONS/zhangdf/sdk/3505/GL5118_WIFI/samples/storyapp/outdir/cmd.txt"))
 
 
 
